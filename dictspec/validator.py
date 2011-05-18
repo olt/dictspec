7eb9dff11dd3ffc4f087f1ad6808d0654b5c4608
Oliver Tonnhofer <olt@bogosoft.com>
2011-05-18 10:13:41 +0200

    move validator functions into class

--- dictspec/validator.py
+++ dictspec/validator.py
@@ -22,68 +22,77 @@ class Context(object):
     def current_pos(self):
         return ''.join(self.obj_pos).lstrip('.')
 
-def validate(spec, data, context=None):
-    if context is None:
-        context = Context()
-    
-    if hasattr(spec, 'subspec'):
-        spec = spec.subspec(data)
-    
-    # store spec for recursive specs in context
-    if isinstance(spec, recursive):
-        if not context.recurse_spec:
-            context.recurse_spec = spec.spec
-        spec = context.recurse_spec
-    
-    if isinstance(spec, anything):
-        return
-    
-    if isinstance(spec, one_off):
-        # check if at least one spec type matches
-        for subspec in spec.specs:
-            if (hasattr(subspec, 'compare_type') and subspec.compare_type(data)
-                or isinstance(data, type(subspec))):
-                spec = subspec
-                break
-        else:
-            raise ValueError("'%s' in %s not of any type %r" % (data, context.current_pos, map(type, spec.specs)))
-    elif hasattr(spec, 'compare_type'):
-        if not spec.compare_type(data):
-            raise ValueError("'%s' in %s not of type %s" % (data, context.current_pos, type(spec)))
-    elif not isinstance(data, type(spec)):
-        raise ValueError("'%s' in %s not of type %s" % (data, context.current_pos, type(spec)))
-    
-    # recurse in dicts and lists
-    if isinstance(spec, dict):
-        validate_dict(spec, data, context)
-    elif isinstance(spec, list):
-        validate_list(spec, data, context)
 
-def validate_dict(spec, data, context):
-    accept_any_key = False
-    any_key_spec = None
-    for k in spec.iterkeys():
-        if isinstance(k, required):
-            if k not in data:
-                raise ValueError("missing '%s' not in %s" % (k, context.current_pos))
-        if isinstance(k, anything):
-            accept_any_key = True
-            any_key_spec = spec[k]
+def validate(spec, data):
+    return Validator(spec).validate(data)
 
-    for k, v in data.iteritems():
-        if accept_any_key:
-            with context.pos('.' + k):
-                validate(any_key_spec, v, context)
+class Validator(object):
+    def __init__(self, spec):
+        self.context = Context()
+        self.complete_spec = spec
+    
+    def validate(self, data):
+        return self._validate_part(self.complete_spec, data)
 
-        else:
-            if k not in spec:
-                raise ValueError("unknown '%s' in %s" % (k, context.current_pos))
-            with context.pos('.' + k):
-                validate(spec[k], v, context)
+    def _validate_part(self, spec, data):
+        if hasattr(spec, 'subspec'):
+            spec = spec.subspec(data)
+    
+        # store spec for recursive specs in context
+        if isinstance(spec, recursive):
+            if not self.context.recurse_spec:
+                self.context.recurse_spec = spec.spec
+            spec = self.context.recurse_spec
+    
+        if isinstance(spec, anything):
+            return
+    
+        if isinstance(spec, one_off):
+            # check if at least one spec type matches
+            for subspec in spec.specs:
+                if (hasattr(subspec, 'compare_type') and subspec.compare_type(data)
+                    or isinstance(data, type(subspec))):
+                    spec = subspec
+                    break
+            else:
+                raise ValueError("'%s' in %s not of any type %r" % (data, self.context.current_pos, map(type, spec.specs)))
+        elif hasattr(spec, 'compare_type'):
+            if not spec.compare_type(data):
+                raise ValueError("'%s' in %s not of type %s" % (data, self.context.current_pos, type(spec)))
+        elif not isinstance(data, type(spec)):
+            raise ValueError("'%s' in %s not of type %s" % (data, self.context.current_pos, type(spec)))
+    
+        # recurse in dicts and lists
+        if isinstance(spec, dict):
+            self._validate_dict(spec, data)
+        elif isinstance(spec, list):
+            self._validate_list(spec, data)
 
-def validate_list(spec, data, context):
-    assert len(spec) == 1
-    for i, v in enumerate(data):
-        with context.pos('[%d]' % i):
-            validate(spec[0], v, context)
+    def _validate_dict(self, spec, data):
+        accept_any_key = False
+        any_key_spec = None
+        for k in spec.iterkeys():
+            if isinstance(k, required):
+                if k not in data:
+                    raise ValueError("missing '%s' not in %s" % (k, self.context.current_pos))
+            if isinstance(k, anything):
+                accept_any_key = True
+                any_key_spec = spec[k]
+
+        for k, v in data.iteritems():
+            if accept_any_key:
+                with self.context.pos('.' + k):
+                    self._validate_part(any_key_spec, v)
+
+            else:
+                if k not in spec:
+                    raise ValueError("unknown '%s' in %s" % (k, self.context.current_pos))
+                with self.context.pos('.' + k):
+                    self._validate_part(spec[k], v)
+
+    def _validate_list(self, spec, data):
+        assert len(spec) == 1
+        for i, v in enumerate(data):
+            with self.context.pos('[%d]' % i):
+                self._validate_part(spec[0], v)
         
